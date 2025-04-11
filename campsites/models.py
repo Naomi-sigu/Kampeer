9ff77148e63a5976b59cc0bef2e9c02fb27db4b4
Naomi Sigu <siguneema@gmail.com>
2025-04-11 16:13:24 +0300

    created a model

--- campsites/models.py
+++ campsites/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Campsites(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+    description = models.TextField()
+    amenities = models.CharField(max_length=255, help_text="Comma-separated list")
+    price_per_night = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    
+
+    def __str__(self):
+        return self.name
